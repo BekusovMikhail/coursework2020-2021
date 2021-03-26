01c0dbc2ee9724ac462e27f451d4d2aba9e36ef6
Mihail <mabekusov@edu.hse.ru>
2021-03-27 00:35:22 +0300

    Update 27.03

--- generator.py
+++ generator.py
@@ -21,14 +21,18 @@ def AddWordsToData(function=workwithfiles.parstext_on_1_word, file_pickle_from_a
 
 
 def create_probs_output():
-    file = open("probsoutput_1word", "w")
-    file = open("probsoutput_2word", "w")
-    file = open("probsoutput_3word", "w")
-    file_names = ["Anna Karenina.txt",
-                  "20000 Leagues Under the Sea.txt",
+    open("probsoutput_1word", "w")
+    open("probsoutput_2word", "w")
+    open("probsoutput_3word", "w")
+    open("probsoutput_4word", "w")
+    file_names = ["20000 Leagues Under the Sea.txt",
+                  "Anna Karenina.txt",
+                  "The Count of Monte Cristo.txt",
                   "The Emperor.txt",
                   "The Idiot.txt",
+                  "The Master and Margarita.txt",
                   "The Mysterious Island.txt",
+                  "The Three Musketeers.txt",
                   "The War of the Worlds.txt",
                   "TheBrothersKaramazov.txt",
                   "WarandPeaceWoNl.txt"]
@@ -36,71 +40,139 @@ def create_probs_output():
         AddWordsToData(workwithfiles.parstext_on_1_word, "probsoutput_1word", i)
         AddWordsToData(workwithfiles.parstext_on_2_word, "probsoutput_2word", i)
         AddWordsToData(workwithfiles.parstext_on_3_word, "probsoutput_3word", i)
+        AddWordsToData(workwithfiles.parstext_on_4_word, "probsoutput_4word", i)
+        print(i)
 
 
 # create_probs_output()
 genprobs = {}
 
 
-def GenerateString(first_string, second_string="", third_string="", window=1, number_of_words=10):
-    if window == 1:
-        probs = parsprobs("probsoutput_1word")
-        genprobs = probs.copy()
-        HMM = [first_string]
-        for i in range(number_of_words):
-            flag = 0
-            while flag == 0:
-                try:
-                    # tmp = sorted(list(probs[HMM[i]].items()), key=lambda x: x[1], reverse=True)[0][0]
-                    tmp = sorted(list(probs[HMM[i]].items()), key=lambda x: x[1], reverse=True)[random.randint(0, 9)][0]
-                    flag = 1
-                except:
-                    flag = 0
-            HMM += [tmp]
-            del probs[HMM[i]][tmp]
-        HMM = " ".join(HMM)
-        print(HMM)
-    elif window == 2:
-        probs = parsprobs("probsoutput_2word")
-        genprobs = probs.copy()
-        HMM = [first_string, second_string]
-        for i in range(1, number_of_words):
-            flag = 0
-            # tmp = sorted(list(probs[(HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
-            while flag == 0:
-                try:
-                    # tmp = sorted(list(probs[(HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
-                    tmp = sorted(list(probs[(HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[
-                        random.randint(0, 10)][0]
-                    flag = 1
-                except:
-                    flag = 0
+def GenerateString(first_string, second_string="", third_string="", fourth_string="", window=1, number_of_words=10,
+                   combinate=False, chance1=0.7, chance2=0.15, chance3=0.45):
+    if not combinate:
+        if window == 1:
+            probs = parsprobs("probsoutput_1word")
 
-            HMM += [tmp]
-            del probs[(HMM[i - 1], HMM[i])][tmp]
+            HMM = [first_string]
+            for i in range(number_of_words):
+                flag = 0
+                while flag == 0:
+                    try:
+                        # tmp = sorted(list(probs[HMM[i]].items()), key=lambda x: x[1], reverse=True)[0][0]
+                        tmp = \
+                            sorted(list(probs[HMM[i]].items()), key=lambda x: x[1], reverse=True)[random.randint(0, 9)][
+                                0]
+                        flag = 1
+                    except:
+                        flag = 0
+                HMM += [tmp]
+                del probs[HMM[i]][tmp]
+            HMM = " ".join(HMM)
+            return HMM
+        elif window == 2:
+            probs = parsprobs("probsoutput_2word")
+
+            if not second_string:
+                second_string = GenerateString(first_string, window=1, number_of_words=1).split()[-1]
+            HMM = [first_string, second_string]
+            for i in range(1, number_of_words):
+                flag = 0
+                # tmp = sorted(list(probs[(HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
+                while flag == 0:
+                    try:
+                        # tmp = sorted(list(probs[(HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
+                        tmp = sorted(list(probs[(HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[
+                            random.randint(0, 10)][0]
+                        flag = 1
+                    except:
+                        flag = 0
+
+                HMM += [tmp]
+                del probs[(HMM[i - 1], HMM[i])][tmp]
+            HMM = " ".join(HMM)
+            return HMM
+        elif window == 3:
+            probs = parsprobs("probsoutput_3word")
+
+            if not second_string:
+                second_string = GenerateString(first_string, window=1, number_of_words=1).split()[-1]
+            if not third_string:
+                third_string = GenerateString(first_string, second_string, window=2, number_of_words=2).split()[-1]
+            HMM = [first_string, second_string, third_string]
+            for i in range(2, number_of_words):
+                flag = 0
+                while flag == 0:
+                    try:
+                        # tmp = sorted(list(probs[(HMM[i - 2], HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
+                        tmp = \
+                            sorted(list(probs[(HMM[i - 2], HMM[i - 1], HMM[i])].items()), key=lambda x: x[1],
+                                   reverse=True)[
+                                random.randint(0, 5)][0]
+                        flag = 1
+                    except:
+                        flag = 0
+                HMM += [tmp]
+                del probs[(HMM[i - 2], HMM[i - 1], HMM[i])][tmp]
+            HMM = " ".join(HMM)
+            return HMM
+        elif window == 4:
+            probs = parsprobs("probsoutput_4word")
+            if not second_string:
+                second_string = GenerateString(first_string, window=1, number_of_words=1).split()[-1]
+            if not third_string:
+                third_string = GenerateString(first_string, second_string, window=2, number_of_words=2).split()[-1]
+            if not fourth_string:
+                fourth_string = GenerateString(first_string, second_string, third_string, window=3,
+                                               number_of_words=3).split()[-1]
+            HMM = [first_string, second_string, third_string, fourth_string]
+            for i in range(3, number_of_words):
+                flag = 0
+                while flag == 0:
+                    try:
+                        # tmp = sorted(list(probs[(HMM[i - 2], HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
+                        tmp = \
+                            sorted(list(probs[(HMM[i - 3], HMM[i - 2], HMM[i - 1], HMM[i])].items()),
+                                   key=lambda x: x[1], reverse=True)[random.randint(0, 5)][0]
+                        flag = 1
+                    except:
+                        flag = 0
+                HMM += [tmp]
+                del probs[(HMM[i - 3], HMM[i - 2], HMM[i - 1], HMM[i])][tmp]
+            HMM = " ".join(HMM)
+            return HMM
+
+    else:
+        if not second_string:
+            second_string = GenerateString(first_string, window=1, number_of_words=1).split()[-1]
+        if not third_string:
+            third_string = GenerateString(first_string, second_string, window=2, number_of_words=2).split()[-1]
+        if not fourth_string:
+            fourth_string = GenerateString(first_string, second_string, third_string, window=3,
+                                           number_of_words=3).split()[-1]
+        HMM = [first_string, second_string, third_string, fourth_string]
+        for i in range(3, number_of_words):
+            chance = random.random()
+            if chance <= chance1:
+                HMM.append(GenerateString(first_string=HMM[-1], window=1, number_of_words=1).split()[-1])
+            elif chance <= chance1 + chance2:
+                HMM.append(
+                    GenerateString(first_string=HMM[-2], second_string=HMM[-1],
+                                   window=2, number_of_words=2).split()[-1])
+            elif chance <= chance1 + chance2 + chance3:
+                HMM.append(
+                    GenerateString(first_string=HMM[-3], second_string=HMM[-2], third_string=HMM[-1], window=3,
+                                   number_of_words=3).split()[-1])
+            else:
+                HMM.append(
+                    GenerateString(first_string=HMM[-4], second_string=HMM[-3], third_string=HMM[-2],
+                                   fourth_string=HMM[-1], window=3, number_of_words=3).split()[-1])
         HMM = " ".join(HMM)
-        print(HMM)
-    elif window == 3:
-        probs = parsprobs("probsoutput_3word")
-        genprobs = probs.copy()
-        HMM = [first_string, second_string, third_string]
-        for i in range(2, number_of_words):
-            flag = 0
-            while flag == 0:
-                try:
-                    # tmp = sorted(list(probs[(HMM[i - 2], HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[0][0]
-                    tmp = \
-                        sorted(list(probs[(HMM[i - 2], HMM[i - 1], HMM[i])].items()), key=lambda x: x[1], reverse=True)[
-                            random.randint(0, 5)][0]
-                    flag = 1
-                except:
-                    flag = 0
-            HMM += [tmp]
-            # del probs[(HMM[i - 2], HMM[i - 1], HMM[i])][tmp]
-        HMM = " ".join(HMM)
-        print(HMM)
+        return HMM
 
 
-GenerateString("i", window=1)
-GenerateString("i", second_string="have", window=2)
-GenerateString("i", second_string="want", third_string="to", window=3, number_of_words=15)
+# print(GenerateString("i", window=1, number_of_words=15))
+# print(GenerateString("i", second_string="have", window=2, number_of_words=2))
+# print(GenerateString("i", second_string="want", third_string="to", window=3, number_of_words=3))
+print(GenerateString("i", window=4, number_of_words=10))
+# print(GenerateString("i", number_of_words=15, combinate=True))
--- workwithfiles.py
+++ workwithfiles.py
@@ -6,8 +6,20 @@ import re
 def tokenize(text):
     text = text.lower()
     words = nltk.word_tokenize(text)
-    text = [word.lower() for word in words if re.findall(r'[A-Za-z]', word)]
+    text = [word.lower() for word in words if re.findall(r'\b[A-Za-z]+\b', word) and word.isalpha()]
     return text
+
+
+def form_genprobs(probs, genprobs):
+    for i in range(len(probs)):
+        if probs[i][0] in genprobs:
+            if probs[i][1] in genprobs[probs[i][0]]:
+                genprobs[probs[i][0]][probs[i][1]] += 1
+            else:
+                genprobs[probs[i][0]][probs[i][1]] = 1
+        else:
+            genprobs[probs[i][0]] = {probs[i][1]: 1}
+    return genprobs
 
 
 def parstext_on_1_word(path, genprobs={}):
@@ -16,27 +28,8 @@ def parstext_on_1_word(path, genprobs={}):
     tokens = tokenize(text)
 
     probs = [[tokens[i], tokens[i + 1]] for i in range(len(tokens) - 1)]
-    for i in range(len(probs)):
-        probs[i] = probs[i][0] + "#" + probs[i][1]
 
-    print(len(probs))
-
-    for i in range(len(probs)):
-        probs[i] = probs[i].split("#")
-
-    probs.sort(key=lambda x: x[0])
-    # print(probs)
-
-    for i in range(len(probs)):
-        if probs[i][0] in genprobs:
-            if probs[i][1] in genprobs[probs[i][0]]:
-                genprobs[probs[i][0]][probs[i][1]] += 1
-            else:
-                genprobs[probs[i][0]][probs[i][1]] = 1
-        else:
-            genprobs[probs[i][0]] = {probs[i][1]: 1}
-
-    return genprobs
+    return form_genprobs(probs, genprobs)
 
 
 def parstext_on_2_word(path, genprobs={}):
@@ -44,25 +37,9 @@ def parstext_on_2_word(path, genprobs={}):
     text = file.read()
     tokens = tokenize(text)
 
-    # print(tokens)
-
     probs = [[(tokens[i], tokens[i + 1]), tokens[i + 2]] for i in range(len(tokens) - 2)]
 
-    # print(len(probs))
-
-    probs.sort(key=lambda x: x[0][0])
-    # print(probs)
-
-    for i in range(len(probs)):
-        if probs[i][0] in genprobs:
-            if probs[i][1] in genprobs[probs[i][0]]:
-                genprobs[probs[i][0]][probs[i][1]] += 1
-            else:
-                genprobs[probs[i][0]][probs[i][1]] = 1
-        else:
-            genprobs[probs[i][0]] = {probs[i][1]: 1}
-
-    return genprobs
+    return form_genprobs(probs, genprobs)
 
 
 def parstext_on_3_word(path, genprobs={}):
@@ -75,16 +52,20 @@ def parstext_on_3_word(path, genprobs={}):
     # probs.sort(key=lambda x: x[0][0])
     # print(probs)
 
-    for i in range(len(probs)):
-        if probs[i][0] in genprobs:
-            if probs[i][1] in genprobs[probs[i][0]]:
-                genprobs[probs[i][0]][probs[i][1]] += 1
-            else:
-                genprobs[probs[i][0]][probs[i][1]] = 1
-        else:
-            genprobs[probs[i][0]] = {probs[i][1]: 1}
+    return form_genprobs(probs, genprobs)
 
-    return genprobs
+
+def parstext_on_4_word(path, genprobs={}):
+    file = open(path)
+    text = file.read()
+    tokens = tokenize(text)
+
+    probs = [[(tokens[i], tokens[i + 1], tokens[i + 2], tokens[i + 3]), tokens[i + 4]] for i in range(len(tokens) - 4)]
+
+    # probs.sort(key=lambda x: x[0][0])
+    # print(probs)
+
+    return form_genprobs(probs, genprobs)
 
 
 def PrintInOutput(file, probs):
