6c8c1aa1e5c12a806660153b8145957764a84903
Mihail <mabekusov@edu.hse.ru>
2021-04-19 13:23:17 +0300

    Add print_score function

--- grading.py
+++ grading.py
@@ -1,31 +1,31 @@
 import kenlm
 import generator
 
+
 # import workwithfiles
 
-model = kenlm.Model('full_text_for_score.binary')
+
+def print_score(string):
+    model = kenlm.Model('full_text_for_score.binary')
+    z = model.score(string, eos=True, bos=True)
+    print(string, z, 10 ** z, sep='\n')
+
 
 tmp = "xxx xxx xxx xxx xxx xxx xxx xxx xxx xxx xxx"
-print(tmp)
-print(model.score(tmp, eos=True, bos=True))
+print_score(tmp)
 print("--------------------------------------------------------------------")
 
 for i in ["hi", "i", "you", "hello", "game"]:
-    tmp1 = generator.GenerateString(i, window=1, number_of_words=10)
-    print(tmp1)
-    print(model.score(tmp1, eos=True, bos=True))
-    tmp2 = generator.GenerateString(i, window=2, number_of_words=10)
-    print(tmp2)
-    print(model.score(tmp2, eos=True, bos=True))
-    tmp3 = generator.GenerateString(i, window=3, number_of_words=10)
-    print(tmp3)
-    print(model.score(tmp3, eos=True, bos=True))
-    tmp4 = generator.GenerateString(i, window=4, number_of_words=10)
-    print(tmp4)
-    print(model.score(tmp4, eos=True, bos=True))
-    tmp5 = generator.GenerateString(i, combinate=True, number_of_words=10)
-    print(tmp5)
-    print(model.score(tmp5, eos=True, bos=True))
+    tmp = generator.GenerateString(i, window=1, number_of_words=10)
+    print_score(tmp)
+    tmp = generator.GenerateString(i, window=2, number_of_words=10)
+    print_score(tmp)
+    tmp = generator.GenerateString(i, window=3, number_of_words=10)
+    print_score(tmp)
+    tmp = generator.GenerateString(i, window=4, number_of_words=10)
+    print_score(tmp)
+    tmp = generator.GenerateString(i, combinate=True, number_of_words=10)
+    print_score(tmp)
     print("--------------------------------------------------------------------")
 
 # tmpfile = open("full_text_tokenized.txt", "w")
